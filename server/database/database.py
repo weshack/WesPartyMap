ecbe80654e1683ce012a1a1f2795b424e929a7a9
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2013-09-06 17:01:00 -0400

    Database stuff pretty much getting there

--- server/database/database.py
+++ server/database/database.py
@@ -4,16 +4,21 @@ from datetime import datetime
 import time
 import logging
 import json
+import math
+
+
+#user = {'name':'John', 'homeLat':'46', 'homeLong':'46'}
 
 def connect_database():
 	return sqlite3.connect('database/database.db')
+#conn = connect_database()
 
 def add_user(conn, user):
 	# User is a dictionary {'name':'', 'homeLat':'', 'homeLong':''}
 	c = conn.cursor()
 	c.execute("insert into userlist (Name, HomeLat, HomeLong) VALUES ({0},{1},{2})".format(json.dumps(user['name']), json.dumps(user['homeLat']), json.dumps(user['homeLong'])))
-	#return uid
 	conn.commit()
+	return c.lastrowid
 
 def delete_user(conn, userID):
 	c = conn.cursor()
@@ -21,11 +26,27 @@ def delete_user(conn, userID):
 	conn.commit()
 
 def check_in(conn, userID, Lat, Long, Time):
-	#if userID location > 100m from Lat Long
 	c = conn.cursor()
-	c.execute("insert into checkin (Latitude, Longitude, Time) VALUES ({0},{1},{2})".format(json.dumps(Lat), json.dumps(Long), Time))
+	c.execute("insert into checkins (Latitude, Longitude, Time) VALUES ({0},{1},{2})".format(json.dumps(Lat), json.dumps(Long), Time))
 	conn.commit()
-	return "200 OK"
+	return ''
 
 def get_home(conn, userID):
 	c = conn.cursor()
+	user = c.execute("select * from userlist where UID={0}".format(userID)).next()
+	return [user[2], user[3]] #lat, long of home
+
+def get_all_checkins(conn):
+	c = conn.cursor()
+	result = c.execute("select * from checkins")
+	checkins = []
+	for res in result:
+		check = {'latitude': res[1],'longitude': res[2],'time': res[3]}
+		checkins.append(check)
+	return checkins
+
+def purge_check_ins(conn, time, timeago): #Need to test this
+	c = conn.cursor()
+	c.execute("delete from checkins where Time < {0}".format(timeago))
+	#c.commit()
+	return ''
--- server/server.py
+++ server/server.py
@@ -1,32 +1,64 @@
 from flask import *
 from database import *
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import mktime
-
+import simplejson
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-    
 @app.before_request
 def before_request():
 	database.db = connect_database()
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route('/checkin/<user>/<latitude>/<longitude>')
 def checkIn(user, latitude, longitude):
+	#purgeCheckIns()
+	if metersBetween([latitude, longitude], get_home(conn, user)) >= 100:
+		now = datetime.utcnow()
+		time = mktime(now.timetuple())
+		print(str(user) + str(latitude) + str(longitude) + str(time))
+		check_in(database.db, user, latitude, longitude, time)
+		return ''
+	else:
+		return simplejson.dumps('at home')
+def purgeCheckIns():
 	now = datetime.utcnow()
+	ago = now - timedelta(hours=2)
 	time = mktime(now.timetuple())
-	check_in(database.db, user, latitude, longitude, time)
+	timeago = mktime(ago.timetuple())
+	purge_check_ins(database.db, time, timeago)
+
+def metersBetween(coord1, coord2):
+	lat1 = coord1[0]
+	long1 = coord1[1]
+	lat2 = coord2[0]
+	long2 = coord2[1]
+	degrees_to_radians = math.pi/180.0
+	phi1 = (90.0 - lat1)*degrees_to_radians
+	phi2 = (90.0 - lat2)*degrees_to_radians
+	theta1 = long1*degrees_to_radians
+	theta2 = long2*degrees_to_radians
+	cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + math.cos(phi1)*math.cos(phi2))
+	arc = math.acos( cos )
+	return (arc*6378100)
 
 @app.route('/user/add/<name>/<homeLat>/<homeLong>')
 def addUser(name, homeLat, homeLong):
 	user = {'name':name,'homeLat':homeLat,'homeLong':homeLong}
-	add_user(database.db, user)
+	return simplejson.dumps({'uid': str(add_user(database.db, user))})
+	
 
 @app.route('/user/delete/<userID>')
 def deleteUser(userID):
 	delete_user(database.db, userID)
+
+@app.route('/checkins')
+def getCheckIns():
+	return simplejson.dumps(get_all_checkins(database.db))
+
 
 if __name__ == "__main__":
     app.secret_key = 'gbn98423jieu394jrfk9je92hk'
