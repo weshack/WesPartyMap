bd44b89898ceb2dc72577474d21393dcae65f869
jlashner <jlashner@wesleyan.edu>
2013-09-06 18:53:31 -0500

    hi sam

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -13,6 +13,46 @@ var loadPoints = function(){
 	});
 };
 
+
+var drawScreen = function (context) {
+    context.fillStyle = "#ffffff";
+    context.fillRect(0,0,200,100);
+    
+    var xi = 10, yi = 110;
+    var gwidth = 180, gheight = 180; 
+    context.beginPath();
+    context.lineWidth = "1";
+    context.rect(xi, yi, gwidth, gheight);
+    context.stroke();
+    
+    context.strokeStyle = "#aaaaaa";
+    for (var i = 1; i < 10;i++){
+        context.beginPath();
+        context.moveTo(xi + i*(gwidth/10), yi)
+        context.lineTo(xi + i*(gwidth/10), yi + gheight);
+        context.stroke();
+    
+        context.beginPath();
+        context.moveTo(xi, yi + i*(gheight/10));
+        context.lineTo(xi + gwidth, yi + i*(gheight/10));
+        context.stroke();
+
+    }
+    
+
+}
+
+var canvasApp = function () {
+    console.log("Drawing");
+
+    var theCanvas = document.getElementById("canvas");
+    var context = theCanvas.getContext("2d");
+    
+    console.log("Drawing Canvas");
+
+    drawScreen(context); 
+}
+
 $(function(){
     var mapOptions = {
         zoom: 16,
@@ -41,14 +81,18 @@ $(function(){
 			});
 			(function (name){
 				google.maps.event.addListener(p, 'click', function(){
+                                        
 					console.log(name)
 					$("#info-pane").css('left','')
 					setTimeout(function(){
 						$("#info-pane").css('left', '80%')
 						$("#info-pane .name").html(name)
+                                                setTimeout(canvasApp, 1000);
 					}, 1000);
 				});
 			})(response[i]['name']);
 		}
 	});
 });
+
+
