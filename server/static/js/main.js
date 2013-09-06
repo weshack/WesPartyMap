e43ce769666890d097699c616881ee26abc6d1cb
mdietz94 <mdietz@wesleyan.edu>
2013-09-06 13:32:38 -0400

    updates

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,9 +1,28 @@
+var currentPoints = []
+
 var loadPoints = function(){
-	$.getJSON("/checkins", function(response){
+	$.getJSON("/checkins").done(function(response){
 		ret = [];
 		for (var i = 0; i < response.length; i++){
-			ret.push({new google.maps.LatLng(response[i]['latitude'],response[i]['longitude'])});
+			ret.push(new google.maps.LatLng(response[i]['latitude'],response[i]['longitude']));
 		}
-		return ret;
+		currentPoints = ret;
 	});
 };
+
+$(function(){
+	var map, pointarray, heatmap;
+	loadPoints();
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(41.555, -72.65957),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    var pointArray = new google.maps.MVCArray(currentPoints);
+    heatmap = new google.maps.visualization.HeatmapLayer({
+    	data: pointArray
+    });
+    heatmap.setMap(map)
+});
