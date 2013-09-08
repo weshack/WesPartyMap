2c6a92ce092065ae75280659302f2268b10072ac
mdietz94 <mdietz@wesleyan.edu>
2013-09-08 06:31:21 -0400

    updated android app, updated apk

--- android_app/src/edu/wesleyan/wespartymap/MainActivity.java
+++ android_app/src/edu/wesleyan/wespartymap/MainActivity.java
@@ -1,12 +1,10 @@
 package edu.wesleyan.wespartymap;
 
-import java.io.File;
-
-import edu.wesleyan.wespartymap.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 	
@@ -15,15 +13,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        File file = getFileStreamPath("userid.info");
-        if (!file.exists()){
-        	TextView textView = (TextView)findViewById(R.id.text);
-        	textView.setText("Registering your phone with WesPartyMap...");
-        	RegisterUser reg = new RegisterUser(this);
-        	reg.execute();
-        } else {
-        	Intent mServiceIntent = new Intent(this, LocationCollector.class);
-        	startService(mServiceIntent);
-        }
+        Intent mServiceIntent = new Intent(this, LocationCollector.class);
+        startService(mServiceIntent);
+        WebView wv = (WebView)findViewById(R.id.webview);
+        WebSettings ws = wv.getSettings();
+        ws.setJavaScriptEnabled(true);
+        wv.loadUrl("http://wespartymap.com");
     }   
 }
