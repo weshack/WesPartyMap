4a82c77ae67add36a1398271164fe2440087c5ef
Max Dietz <mdietz@wesleyan.edu>
2013-09-09 18:43:42 +0000

    gunicorn settings

---
+++ server/gunicorn.py
@@ -0,0 +1,14 @@
+import os
+def numCPUs():
+    if not hasattr(os, "sysconf"):
+        raise RuntimeError("No sysconf detected.")
+    return os.sysconf("SC_NPROCESSORS_ONLN")
+bind = "0.0.0.0:80"
+workers = numCPUs() * 2 + 1
+backlog = 2048
+#worker_class ="sync"
+worker_class =  "gevent"
+debug = True
+daemon = True
+pidfile ="/tmp/gunicorn.pid"
+logfile ="/tmp/gunicorn.log"
